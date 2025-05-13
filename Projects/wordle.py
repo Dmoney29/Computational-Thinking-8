be97d3730796788efc720fbac9523e3088767aaa
Dmoney29 <dallasdixon@seattleacademy.org>
2025-05-13 17:34:47 +0000

    finished

--- Projects/wordle.py
+++ Projects/wordle.py
@@ -18,14 +18,37 @@ for i in range(6):
     else:
         output += "⬛"
 
-         # Secound letter 
+         # Second letter 
     if guess_word[1] == hidden_word[1]:
         output += "🟩"
     elif guess_word[1] in hidden_word:
         output += "🟨"
     else:
         output += "⬛"
+
+         # Third letter
+    if guess_word[2] == hidden_word[2]:
+        output += "🟩"
+    elif guess_word[2] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+         # First letter (in python, counting starts at 0 not 1)
+    if guess_word[3] == hidden_word[3]:
+        output += "🟩"
+    elif guess_word[3] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
     
+     # First letter (in python, counting starts at 0 not 1)
+    if guess_word[4] == hidden_word[4]:
+        output += "🟩"
+    elif guess_word[4] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
 
     # Result
     print(output)
